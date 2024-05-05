54b9eaf724cff6b5824431ffc35c200b7957431e
muhammadfathurraiyan <raiyanfathur@gmail.com>
2024-05-05 22:17:05 +0700

    update

--- api/index.js
+++ api/index.js
@@ -11,30 +11,12 @@ app.use(cors());
 
 app.post("/api/:query", async (req, res) => {
   try {
-    const response = await axios.post(
-      process.env.API_ENDPOINT,
-      {
-        model: "gpt-3.5-turbo",
-        messages: [
-          {
-            role: "user",
-            content: req.params.query,
-          },
-        ],
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + process.env.API_KEY,
-        },
-      }
-    );
-
-    // Send the response from the API back to the client
-    // console.log(response.data?.choices[0]?.message?.content)
+    const response = await axios.post(process.env.API_ENDPOINT, {
+      prompt: req.params.query + ",jawab secara singkat",
+    });
+    // console.log(response.data?.choices[0]?.message?.content);
     res.status(200).json(response.data?.choices[0]?.message?.content);
   } catch (error) {
-    // Handle any errors that occur during the Axios request
     console.error("Error making API request:", error);
   }
 });
